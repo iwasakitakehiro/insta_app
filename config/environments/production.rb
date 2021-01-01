eeff7611dafa311214fb34cfa1fa3f28d87e3728
iwasakitakehiro <iwasakitakehiro2@gmail.com>
2021-01-02 02:25:27 +0900

    connect google analytics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'quiet-meadow-35300.herokuapp.com'
+  host = 'calm-tundra-37972.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
